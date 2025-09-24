25874bba41a0a3a7717d5b2bf191f4c0a8c26643
MathildeDamsgaard <eaa25madh@students.eaaa.dk>
2025-09-24 14:00:58 +0200

    Nye ændringer

--- app.js
+++ app.js
@@ -3,11 +3,12 @@ document.body.innerHTML += "";
 console.log("Hello Multimedia Designers");
 console.log("Hi Students!");
 console.log("This is your first JavaScript App 🎉");
-function sayHello() {}
 
 function sayHello() {
   document.querySelector("h3").textContent =
-    "Hej " + document.querySelector("input").value;
+    "Hej " +
+    document.querySelector("#name").value +
+    document.querySelector("#age").value;
 }
 
 document.querySelector("button").addEventListener("click", sayHello);
